30356173b706b71c2e18468f341acac0b17484c2
DanilIvasuk <you@example.com>
2021-05-29 19:34:34 +0300

    work

--- app/public/js/menu.js
+++ app/public/js/menu.js
@@ -19,34 +19,44 @@ $(document).ready(function(){
         if ($('body').hasClass('overflow-hidden-body')) {
 
             $('body').removeClass('overflow-hidden-body')
-
+            handlerMenu();
         } else {
             $('body').addClass('overflow-hidden-body')
+            setMenu(true);
+
         }
 
     }
 
 });
 
-
-
-remove();
-function remove() {
-    if($(this).scrollTop()>70){
+function setMenu(isStatic){
+    if(isStatic){
         $('.container-logo').addClass('remove-logo');
         $('.navigation-menu').addClass('navigation-menu-static');
-    }
-    else if ($(this).scrollTop()<70){
+    }else{
         $('.container-logo').removeClass('remove-logo');
         $('.navigation-menu').removeClass('navigation-menu-static');
+    }
+}
+
+handlerMenu();
+function handlerMenu() {
+    if($(this).scrollTop()>70){
+        setMenu(true);
+    }
+    else if ($(this).scrollTop()<70){
+        setMenu(false);
+
 
     }
 }
 
 jQuery(function($) {
     $(window).scroll(function(){
-        remove();
+        handlerMenu();
     })
+    handlerMenu();
 
     $(document).ready(function(){
         $('img').each(function(){
